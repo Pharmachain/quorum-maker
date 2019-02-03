52ee452db6d2607f06bdcd3897368ecd00a9b215
root <jkilfoyle@zagmail.gonzaga.edu>
2019-02-02 16:41:33 -0800

    Fixed node unlock script

--- TestNetwork/modular.sh
+++ TestNetwork/modular.sh
@@ -1,11 +1,13 @@
-nodes="$(find . -name 'node*' -type d -maxdepth 1)"
+nodes="$(find . -maxdepth 1 -name 'node*' -type d)"
 echo $nodes
 
 for node in `find . -name 'node*' -type d -maxdepth 1` ; do
 	echo $node
 	address="$(geth --exec 'personal.listAccounts' attach $node/node/qdata/geth.ipc)"
+	address=${address#"["}
+	address=${address%"]"}
+	address=${address%"\""}
+	address=${address#"\""}
 	echo $address
-	address=${address::-2}
-	address=${address:2}
 	geth --exec "personal.unlockAccount('$address','',100000000)" attach node1/node/qdata/geth.ipc
 done
